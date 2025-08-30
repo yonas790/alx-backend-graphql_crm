c8d30d6c06580506ae897dd45d996d1c8dc8e88d
yonas790 <yoni2752@gmail.com>
2025-08-30 19:04:58 +0300

    Add crm/settings.py with correct CRONJOBS and INSTALLED_APPS

---
+++ crm/settings.py
@@ -0,0 +1,15 @@
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'crm',
+    'django_crontab',
+]
+
+CRONJOBS = [
+    ('*/5 * * * *', 'crm.cron.log_crm_heartbeat'),
+    ('0 */12 * * *', 'crm.cron.update_low_stock'),
+]
